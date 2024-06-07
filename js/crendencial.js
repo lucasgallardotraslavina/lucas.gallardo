54390f994c4bbaa7cc311c8712d64396c44c508e
lucasgallardotraslavina <147518818+lucasgallardotraslavina@users.noreply.github.com>
2024-06-07 09:08:05 -0400

    estructura de formulario(index)
    formulario con 9 campos
    3 tipo text
    1 tipo number
    2 tipo radio
    1tipo check box
    1tipo select

---
+++ js/crendencial.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAZPyRm9CNt2vzxAcLJNPVTkkwopBjVjbo",
+    authDomain: "eva3-6c83b.firebaseapp.com",
+    projectId: "eva3-6c83b",
+    storageBucket: "eva3-6c83b.appspot.com",
+    messagingSenderId: "558158976160",
+    appId: "1:558158976160:web:7fac5e7ef200ae9683e5c5"
+  };
---
+++ js/firebase.js
@@ -0,0 +1,4 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-store.js";
+
+const app = initializeApp(firebaseConfig);
